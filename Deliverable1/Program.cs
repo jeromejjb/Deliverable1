d23db73693849ca0ab4af00fd6543db0cc8e2fa9
jerome <jeromebrown97@gmail.com>
2021-05-17 11:13:19 -0500

    I added some comments to my code

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -19,17 +19,18 @@ namespace Deliverable1
             // asking for a users measurement input
 
             Console.Write("Enter an amount ");
+            // asking user for an amount of measurement
             
             
              milhip = Convert.ToInt32(Console.ReadLine());
              Console.WriteLine("Figet Spinners = {0}", milhip * 3.5);
              Console.WriteLine("Memes = {0}", milhip * 5);
-            
+            // convert measurement if in milhip to Figet spinners and Memes
              
             imperial = Convert.ToChar(Console.ReadLine());
             Console.WriteLine("inches = {0}", imperial * 1);
             Console.WriteLine("feet = {0}", imperial * 1);
-            
+            // convert measurement if in imperial to inches and feet
          
          
          
