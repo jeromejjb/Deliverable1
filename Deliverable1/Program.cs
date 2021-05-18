3805ad199f691d3c75b853e79da37b6f80a26069
jerome <jeromebrown97@gmail.com>
2021-05-17 21:40:22 -0500

    Created loop, finished up if else statement

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -6,34 +6,40 @@ namespace Deliverable1
     {
 
 
-        static int Main(string[] args)
-        {
+        static void Main(string[] args) {
+
+
             string measurementType;
             int milhip;
             int imperial;
+            string userAnswer = "yes";
 
+            while (userAnswer == "yes")
+            {  
+                // asking for a users measurement input
+                Console.Write("Please enter a measurement type ");
+                measurementType = Console.ReadLine();
 
-
-            Console.Write("Please enter a measurement type ");
-            measurementType = Console.ReadLine();
-            // asking for a users measurement input
-
-            Console.Write("Enter an amount ");
-            // asking user for an amount of measurement
-            
-            
-             milhip = Convert.ToInt32(Console.ReadLine());
-             Console.WriteLine("Figet Spinners = {0}", milhip * 3.5);
-             Console.WriteLine("Memes = {0}", milhip * 5);
-            // convert measurement if in milhip to Figet spinners and Memes
-             
-            imperial = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("inches = {0}", imperial * 1);
-            Console.WriteLine("feet = {0}", imperial * 1);
-            // convert measurement if in imperial to inches and feet
-         
-         
-         
+                // asking user for an amount of measurement
+                Console.Write("Enter an amount ");
+               
+                if (measurementType == "milhip")
+                {
+                    // convert measurement if in milhip to Figet spinners and Memes
+                    milhip = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("{0} Figet Spinners", milhip * 3.5);
+                    Console.WriteLine("{0} Memes", milhip * 5);   
+                }
+                else
+                {
+                    // convert measurement if in imperial to inches and feet
+                    imperial = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("{0} inches", imperial * 1);
+                    Console.WriteLine("{0} feet", imperial * 1);
+                }
+                Console.Write("would you like to do another calculation? ");
+                userAnswer = Console.ReadLine();  
+            }
         }
     }
 }
